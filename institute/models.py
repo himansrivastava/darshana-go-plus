08117041c9fa8f89b065ca7fef15dd421a9b1949
Himanshu Srivastava <h.dataconsultancy@gmail.com>
2022-07-27 20:22:15 +0530

    create the Institute model

--- institute/models.py
+++ institute/models.py
@@ -1,1 +1,19 @@
 from django.db import models
+
+
+class Institute(models.Model):
+    """Model class for institue"""
+
+    class Status(models.TextChoices):
+        """Status enum for institue"""
+
+        ACTIVE = ("Active",)
+        INACTIVE = ("InActive",)
+        ABOUTTOEXPIRE = ("AboutToExpire",)
+        CANCELLED = ("Cancelled",)
+
+    id = models.CharField(primary_key=True, max_length=10)
+    name = models.CharField(max_length=100)
+    address = models.CharField(max_length=200)
+    contact_person = models.CharField(max_length=100)
+    status = models.CharField(max_length=20, choices=Status.choices)
