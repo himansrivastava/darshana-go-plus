d3fafcd72c4ba79f3c4799c3ac32e016542674e3
Himanshu Srivastava <h.dataconsultancy@gmail.com>
2022-07-27 20:23:05 +0530

    add urls for Institute api

---
+++ institute/urls.py
@@ -0,0 +1,17 @@
+"""URLs for the institue api"""
+
+from django.urls import path, include
+
+from rest_framework.routers import DefaultRouter
+
+from institute import views
+
+app_name = "instituteapi"
+
+router = DefaultRouter()
+
+router.register("institute", views.InstituteViewSet, basename="institute")
+
+urlpatterns = [
+    path("", include(router.urls)),
+]
