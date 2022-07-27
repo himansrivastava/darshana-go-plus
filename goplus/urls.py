9b0edaa19e8cf838893ab65578b4f4dcce687c64
Himanshu Srivastava <h.dataconsultancy@gmail.com>
2022-07-27 20:23:34 +0530

    configure urls for the project

--- goplus/urls.py
+++ goplus/urls.py
@@ -14,8 +14,31 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="GO PLUS API",
+        default_version="v1",
+        description="API for GO PLUS",
+        contact=openapi.Contact(email="himansrivastava@gmail.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=False,
+    permission_classes=(permissions.IsAdminUser,),
+)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("api/v1/institute/", include("institute.urls")),
+    # API Documentation
+    path(
+        "swagger/",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+    path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
 ]
